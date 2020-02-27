cc5697b613d1b89fc53e65cf1a9ecd706220274d
mo.yu <mo.yu@tendcloud.com>
2020-02-27 10:26:34 +0800

    feat:create

---
+++ src/views/Home.vue
@@ -0,0 +1,74 @@
+<template>
+	<div id="mountNode"></div>
+</template>
+
+<script>
+// @ is an alias to /src
+import G6 from "@antv/g6";
+export default {
+	name: 'Home',
+	components: {
+	},
+	mounted() {
+		this.init();
+	},
+	methods: {
+		init() {
+			const data = {
+				nodes: [{
+					id: "node1",
+					x: 100,
+					y: 200,
+					name: "image",
+					label: "\ue600",
+					itemType: "node",
+					// text: '\ue600',
+					text: "gaah",
+					size: 60,
+					type: "image",
+					image: "image.png",
+				}],
+				edges: []
+			};
+			const graph = new G6.Graph({
+				container: 'mountNode',
+				width: 500,
+				height: 300
+			});
+
+
+			G6.registerNode("image", {
+				draw(cfg, group) {
+					const size = cfg.size;
+
+					group.addShape("circle", {
+						attrs: {
+							r: size,
+							fill: "red",
+						},
+						name: "mage-outline",
+					});
+
+					group.addShape("text", {
+						attrs: {
+							fontFamily: "iconfont",
+							y: 25,
+							textAlign: "center",
+							text: cfg.label,
+							fontSize: 40,
+							fill: "#198abca6"
+						},
+						name: "image-title",
+					});
+				},
+			});
+
+			graph.read(data);
+		}
+	}
+}
+</script>
+
+<style>
+@import '../assets/font/iconfont.css';
+</style>
